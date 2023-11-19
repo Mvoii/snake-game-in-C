13bb27090b7bc66b0b585c568e191740f0524e52
Franklin Mvoi <mvoifranklin@gmail.com>
2023-11-19 21:40:18 +0300

    added to do lists

--- header.h
+++ header.h
@@ -1,15 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+//TODO define snake length
 
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
-//#include <conio.h>
+//#include <conio.h> -- works in windows only
 #include <unistd.h>
 
 void welcomeScreen(void);
 void clrscr(void);
 char waitForAnyKey(void);
 
+//TODO create snake struct
+//TODO implement movement
+//TODO add food randomization function
+//TODO create a collision algo fro 2-D games - vector geometry
+//TODO
 
 #endif
